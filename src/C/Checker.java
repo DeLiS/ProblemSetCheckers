2ec0082fd9cffd117b864009cb8a84bcde9a4130
DeLiS <delis91@gmail.com>
2014-03-12 16:26:28 +0200

    Main method for C checker added

--- src/C/Checker.java
+++ src/C/Checker.java
@@ -9,13 +9,23 @@ import java.util.Scanner;
  * Created by Denis on 12.03.14.
  */
 public class Checker extends AbstractChecker {
+    public static void main(String[] args) {
+
+        Checker checker = new Checker();
+        int resultCode = checker.check(args);
+        System.out.println(checker.getMessage());
+        System.exit(resultCode);
+    }
+
     @Override
     public boolean checkIfAnswerIsCorrect(Scanner input, Scanner output) throws Exception {
         DepthFirstSearch dfs = new DepthFirstSearch(input);
+        dfs.run();
         int numberOfVisitedVertexes = dfs.getNumberOfVertexes();
         String path = dfs.getPath().trim();
 
         int k = output.nextInt();
+        output.nextLine();
         String outputPath = output.nextLine().trim();
 
         return k == numberOfVisitedVertexes && path.equals(outputPath);
